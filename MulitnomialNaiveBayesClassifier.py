d5d6ba8dc161d984b6e0330e4de4b78689b175df
gitkubus <jj.259@icloud.com>
2025-01-16 18:35:49 +0100

    final final

--- MulitnomialNaiveBayesClassifier.py
+++ MulitnomialNaiveBayesClassifier.py
@@ -2,6 +2,7 @@ from collections import Counter, defaultdict
 from math import log
 import numpy as np
 
+    
 class MNBC:
     def __init__(self):
         self.feature_count = defaultdict(lambda: defaultdict(lambda: Counter()))
@@ -9,22 +10,19 @@ class MNBC:
         self.priors = defaultdict(dict)
 
     def fit(self, X, Y):
-        X = np.array(X) 
-        Y = np.array(Y)
         n, n_features = X.shape
         for i in range(n):
             c = Y[i]
-            self.class_count[c]+=1 #++
+            self.class_count[c] #++
             for j in range(n_features):
                 feature = j
-                self.feature_count[c][feature][X[i, j]]+=1 #++
+                self.feature_count[c][feature][X[i, j]] #++
 
         self.class_count = dict(Counter(Y))
         for c in set(Y):
             self.priors[c] = self.class_count[c] / len(X)
 
     def predict(self, X):
-        X = np.array(X) 
         predictions = []
         for i in range(len(X)):
             c_hat = self.predict_proba(X[i])
@@ -33,6 +31,7 @@ class MNBC:
 
     def predict_proba(self, x):
         probabilities = {k: log(v) for k, v in self.priors.items()}
+
         for c in self.class_count:
             for feature in range(len(x)):
                 feature_val = x[feature]
